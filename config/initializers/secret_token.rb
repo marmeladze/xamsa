2e953c6a0e7d7fa6f7f8c776a582cc3624ef01e0
Ali Ismayilov <ali@ismailov.info>
2013-11-16 15:31:28 +0400

    Replace secret token with environment variable.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Xamsa::Application.config.secret_key_base = 'c5dc21e965976000fdc03310c102c834df60dc3c8ef9affabba9791592887715d4961ba8e869326dedff1610c14e37227c887fa6b47d6a489130619e8bbf97db'
+Xamsa::Application.config.secret_key_base = ENV['SECRET_TOKEN']
