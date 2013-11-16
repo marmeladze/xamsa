1f0b01c2e7d0938d760730927e16ee04694eb333
Ali Ismayilov <ali@ismailov.info>
2013-11-16 15:30:49 +0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Xamsa::Application.config.secret_key_base = 'c5dc21e965976000fdc03310c102c834df60dc3c8ef9affabba9791592887715d4961ba8e869326dedff1610c14e37227c887fa6b47d6a489130619e8bbf97db'
