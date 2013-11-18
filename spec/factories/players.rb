4706b2113351fac78d1d29bdbad095f754226957
Ali Ismayilov <ali@ismailov.info>
2013-11-18 00:09:56 -0200

    add required fields

--- spec/factories/players.rb
+++ spec/factories/players.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :player do
     full_name 'Test Player'
+    birthday '30.12.1989'
+    sex true
     email 'example@example.com'
     password 'changeme'
     password_confirmation 'changeme'
