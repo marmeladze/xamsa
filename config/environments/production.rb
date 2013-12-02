f6d814af070c85dd4e36085e0a9bc5d5087c402f
Ali Ismayilov <ali@ismailov.info>
2013-12-02 03:06:07 -0200

    use regex to find bootstrap fonts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Xamsa::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( 'glyphicons-halflings-regular.woff', 'glyphicons-halflings-regular.eot', 'glyphicons-halflings-regular.svg', 'glyphicons-halflings-regular.ttf' )
+  config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
